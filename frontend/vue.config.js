25321a6bd641abb0b57eaa9d43e092d85f13774b
Ian Hulsbus <ian@hulsbus.be>
2022-04-20 17:57:49 +0200

    Reconfigure Docker build process to not include source maps. Generate correct NGINX config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  productionSourceMap: false,
   configureWebpack: {
     devServer: {
       'headers': {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Headers": "content-type, Authorization"
       }
     }
   },
